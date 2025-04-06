49aad6c1d1bf9468dbe45352b7c677973538114a
Nicki tst <kanieleoutiz@gmail.com>
2025-04-06 16:54:50 +0300

    add Announcement model

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
+import atexit
 import os
+from datetime import datetime
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
@@ -12,5 +14,19 @@ DB_NAME = os.getenv("DB_NAME", "postgres")
 DSN = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DSN)
-
 Session = sessionmaker(bind=engine)
+
+class Base(DeclarativeBase):
+    pass
+
+class Announcement(Base):
+    __tablename__ = "announcements"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
+    regisration_time: Mapped[datetime] = mapped_column(nullable=False, func=datetime.now())
+
+Base.metadata.create_all(engine)
+
+atexit.register(engine.dispose)
