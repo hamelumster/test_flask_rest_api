8ca23deca97b0192526059e7619356116f3a0074
Nicki tst <kanieleoutiz@gmail.com>
2025-04-06 16:26:41 +0300

    add models.py (connect to DB)

---
+++ models.py
@@ -0,0 +1,16 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+DB_NAME = os.getenv("DB_NAME", "postgres")
+
+DSN = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DSN)
+
+Session = sessionmaker(bind=engine)
