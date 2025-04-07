d4560599a7653d7cdf87a1f1cfec30ed038cb8ec
Nicki tst <kanieleoutiz@gmail.com>
2025-04-07 15:55:04 +0300

    checking requests: get, post, delete, patch

--- client.py
+++ client.py
@@ -1,7 +1,53 @@
 import requests
 
+# Создаем пользователя
+# user_response = requests.post(
+#     "http://127.0.0.1:5000/api/v1/user",
+#     json={
+#         "username": "test_user",
+#         "password": "password"
+#     }
+# )
+# print(f"user response: {user_response.status_code}, {user_response.json()}")
 
-response = requests.get("http://127.0.0.1:5000/1")
+# Получаем пользователя
+# response = requests.get(
+#     "http://127.0.0.1:5000/api/v1/user/1")
+#
+# print(f"user response: {response.status_code}, {response.text}")
 
-print(response.status_code)
-print(response.text)
+# Создаем объявление (предполагаем, что id = 1)
+# response = requests.post(
+#     "http://127.0.0.1:5000/api/v1/announcement",
+#     json={
+#         "title": "Суп",
+#         "description": "Вкусный",
+#         "owner": 1
+#     }
+# )
+# print(f"announcement response: {response.status_code}, {response.json()}")
+
+# Получаем объявление
+# response = requests.get(
+#     "http://127.0.0.1:5000/api/v1/announcement/2"
+# )
+#
+# print(f"announcement response: {response.status_code}, {response.json()}")
+
+# Удаляем объявление
+# response = requests.delete(
+#     "http://127.0.0.1:5000/api/v1/announcement/1"
+# )
+# print(f"announcement response: {response.status_code}, {response.text}")
+
+# Изменяем объявление
+# response = requests.patch(
+#     "http://127.0.0.1:5000/api/v1/announcement/2",
+#     json={
+#         "title": "Суп",
+#         "description": "Слишком Вкусный!!!",
+#         "owner": 1
+#     }
+# )
+#
+# print(f"announcement response: {response.status_code}, {response.json()}")
