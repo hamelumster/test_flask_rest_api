c8aaab4b6d005475a7a5e76f8ea022108bff4966
Nicki tst <kanieleoutiz@gmail.com>
2025-04-07 14:32:39 +0300

    troubleshooting DSN

--- models.py
+++ models.py
@@ -9,13 +9,11 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column,
 
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
-DSN = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DSN = f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5431/{DB_NAME}"
 
 engine = create_engine(DSN)
 Session = sessionmaker(bind=engine)
