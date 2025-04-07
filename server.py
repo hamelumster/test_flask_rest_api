b3260cac3e7ad15ad59894ae191700d94d487217
Nicki tst <kanieleoutiz@gmail.com>
2025-04-07 11:17:46 +0300

    add url rules

--- server.py
+++ server.py
@@ -4,13 +4,20 @@ from flask.views import MethodView
 app = Flask("app")
 
 class AnnouncementView(MethodView):
-    def get(self):
+    def get(self, announcement_id: int):
         pass
-    def post(self):
+    def post(self, announcement_id: int):
         pass
-    def delete(self):
+    def delete(self, announcement_id: int):
         pass
-    def patch(self):
+    def patch(self, announcement_id: int):
         pass
+
+announcement_view = AnnouncementView.as_view("announcement_view")
+app.add_url_rule("api/v1/announcement", view_func=announcement_view, methods=["POST"])
+app.add_url_rule("api/v1/announcement/<int:announcement_id>",
+                 view_func=announcement_view,
+                 methods=["GET", "DELETE", "PATCH"]
+)
 
 app.run()
