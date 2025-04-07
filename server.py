c1cddf919a0b8324c1f19378e0a30d19f21b17e5
Nicki tst <kanieleoutiz@gmail.com>
2025-04-07 11:02:05 +0300

    add methods: get post delete patch

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
 from flask import Flask, jsonify
-
+from flask.views import MethodView
 
 app = Flask("app")
 
-def start():
-
-    http_response = jsonify({"hi": "i'm here"})
-    http_response.status_code = 201
-    return http_response
-
-app.add_url_rule("/1", view_func=start)
+class AnnouncementView(MethodView):
+    def get(self):
+        pass
+    def post(self):
+        pass
+    def delete(self):
+        pass
+    def patch(self):
+        pass
 
 app.run()
